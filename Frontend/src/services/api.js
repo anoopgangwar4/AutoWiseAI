647322035ad3ae555f6acd059b372f0c5e26d6db
gangwar <gangwaranoop4@gmail.com>
2026-02-08 12:48:31 -0600

    add env file

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,5 +1,8 @@
 
-const API_BASE = "http://localhost:5000";
+// const API_BASE = "http://localhost:5000";
+// const API_BASE = "http://0.0.0.0:8080";
+const API_BASE = import.meta.env.VITE_API_BASE;
+
 
 export async function predictCarPrice(data) {
   const res = await fetch(`${API_BASE}/predict_car`, {
