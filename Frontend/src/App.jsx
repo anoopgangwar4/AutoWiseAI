f9da182bcf3637e9e534ec4d406ddda744443854
gangwar <gangwaranoop4@gmail.com>
2026-02-10 01:44:10 -0600

    Revert "Reapply "updation in ui""
    This reverts commit 4db8e3442bd711de26f4a460a7717e744a7392b7.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,9 +4,7 @@ import CarPredictor from "./components/CarPredictor.jsx";
 import BikePredictor from "./components/BikePredictor.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
-import Chatbot from "./components/Chatbot.jsx";
-import Login from "./components/auth/Login.jsx";
-import SignUp from "./components/auth/SignUp.jsx";
+import ChatBot from "./components/ChatBot.jsx";
 
 export default function App() {
   return (
@@ -14,11 +12,9 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/chatwithus" element={<Chatbot />} />
+        <Route path="/chatwithus" element={<ChatBot />} />
         <Route path="/car" element={<CarPredictor />} />
         <Route path="/bike" element={<BikePredictor />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
       </Routes>
 
       <Footer />
